114c62034bcda67c5efad4a1a37be87ad7f52965
br1-gh <viceofchaos@gmail.com>
2019-08-19 15:13:07 +0200

    Add Pets and contact info to Owners on startup. Closes #33.

--- pet-clinic-web/src/main/java/com/br1/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/br1/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.br1.sfgpetclinic.bootstrap;
 
 import com.br1.sfgpetclinic.model.Owner;
+import com.br1.sfgpetclinic.model.Pet;
 import com.br1.sfgpetclinic.model.PetType;
 import com.br1.sfgpetclinic.model.Vet;
 import com.br1.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.br1.sfgpetclinic.services.PetTypeService;
 import com.br1.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -26,21 +29,41 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         PetType dog = new PetType();
-        dog.setName("Doggoh");
+        dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
-        cat.setName("Cattoh");
+        cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Bickreel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123412341234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Tubble Dr.");
+        owner2.setCity("Miami");
+        owner2.setTelephone("2345235235");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
