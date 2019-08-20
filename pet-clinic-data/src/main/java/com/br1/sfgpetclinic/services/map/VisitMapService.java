a52c908980dcdad05683ac40cd3bb342e7f22697
br1-gh <viceofchaos@gmail.com>
2019-08-20 13:28:31 +0200

    Add Service annotation for Map Impl of visits.

--- pet-clinic-data/src/main/java/com/br1/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/br1/sfgpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package com.br1.sfgpetclinic.services.map;
 
 import com.br1.sfgpetclinic.model.Visit;
 import com.br1.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
