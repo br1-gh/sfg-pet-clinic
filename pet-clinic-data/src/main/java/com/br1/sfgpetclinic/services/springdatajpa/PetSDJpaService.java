68646b665832f97b3f407dfcd93b4f64a57ae049
br1-gh <viceofchaos@gmail.com>
2019-08-20 12:01:39 +0200

    Add spring data jpa service annotations for Pet and PetService.

--- pet-clinic-data/src/main/java/com/br1/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/com/br1/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -3,10 +3,14 @@ package com.br1.sfgpetclinic.services.springdatajpa;
 import com.br1.sfgpetclinic.model.Pet;
 import com.br1.sfgpetclinic.repositories.PetRepository;
 import com.br1.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
     private final PetRepository petRepository;
