24d2034536cac730e3fd20b468dd3d32082120a8
br1-gh <viceofchaos@gmail.com>
2019-08-20 15:47:57 +0200

    Refactor with Project Lombok. Closes #51.

--- pet-clinic-data/src/main/java/com/br1/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/br1/sfgpetclinic/model/Vet.java
@@ -1,9 +1,17 @@
 package com.br1.sfgpetclinic.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -13,12 +21,9 @@ public class Vet extends Person {
         inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
+    @Builder
+    public Vet(Long id, String firstName, String lastName, Set<Specialty> specialties) {
+        super(id, firstName, lastName);
         this.specialties = specialties;
     }
 }
