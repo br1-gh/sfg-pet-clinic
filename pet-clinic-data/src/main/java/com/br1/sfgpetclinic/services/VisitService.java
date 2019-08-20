a876a130001b6cffb6e49e68060fe05f670c2bb9
br1-gh <viceofchaos@gmail.com>
2019-08-20 12:39:00 +0200

    Implement map impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/br1/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.br1.sfgpetclinic.services;
+
+import com.br1.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/br1/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package com.br1.sfgpetclinic.services.map;
+
+import com.br1.sfgpetclinic.model.Visit;
+import com.br1.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null
+                || object.getId() == null
+                || object.getPet().getOwner() == null
+                || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
