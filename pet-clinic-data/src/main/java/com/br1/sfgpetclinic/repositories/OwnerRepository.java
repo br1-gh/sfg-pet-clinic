29cd88e59318a4af15d1bd470c7ba169f9c0b91f
br1-gh <viceofchaos@gmail.com>
2019-08-21 17:45:39 +0200

    Ipml find owner. Closes #54.

--- pet-clinic-data/src/main/java/com/br1/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/br1/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package com.br1.sfgpetclinic.repositories;
 import com.br1.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastName(String lastName);
 }
